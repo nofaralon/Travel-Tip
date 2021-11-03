43eb9a878a56517e2bdf224e4a6327446d0fc90c
Yahav <freeman.yav@gmail.com>
2021-11-03 16:49:56 +0200

    go & render

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,85 +1,99 @@
-import { locService } from './services/loc.service.js'
-import { mapService } from './services/map.service.js'
-import { storage } from './services/storage.service.js'
+import { locService } from "./services/loc.service.js";
+import { mapService } from "./services/map.service.js";
+import { storage } from "./services/storage.service.js";
 
 window.onload = onInit;
 window.onAddMarker = onAddMarker;
 window.onPanTo = onPanTo;
 window.onGetLocs = onGetLocs;
 window.onGetUserPos = onGetUserPos;
+window.onGoPlace=onGoPlace;
 
 function onInit() {
-    mapService.initMap()
-        .then(() => {
-            console.log('Map is ready');
+  mapService
+    .initMap()
+    // .then((res) => {
+    //   console.log("Map is ready",res);
+    // })
+    .then((gMap) => {
+      gMap.addListener("click", (mapsMouseEvent) => {
+        var placeName = prompt("Enter your place name");
+        var location = mapsMouseEvent.latLng.toJSON();
+        console.log(location);
+        var lat = location.lat;
+        var lng = location.lng;
+        var createdAt = Date.now();
+        locService.creatNewLocation(lat, lng, createdAt, placeName);
+        locService.getLocs().then((places)=>{
+            console.log(places)
+            renderPlaces(places);
         })
-        .catch(() => console.log('Error: cannot init map'));
-    var key = locService.getKey()
-    var places = storage.load(key)
-    if (places) renderPlaces(places)
+      });
+    })
+    
+    .catch(() => console.log("Error: cannot init map"));
+  var key = locService.getKey();
+  var places = storage.load(key);
+  if (places) renderPlaces(places);
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos');
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+  console.log("Getting Pos");
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 }
 
 function onAddMarker() {
-    console.log('Adding a marker');
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+  console.log("Adding a marker");
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
 }
 
 function onGetLocs() {
-    locService.getLocs()
-        .then(locs => {
-            console.log('Locations:', locs)
-            document.querySelector('.locs').innerText = JSON.stringify(locs)
-        })
+  locService.getLocs().then((locs) => {
+    console.log("Locations:", locs);
+    document.querySelector(".locs").innerText = JSON.stringify(locs);
+  });
 }
 
 function onGetUserPos() {
-    getPosition()
-        .then(pos => {
-            console.log('User position is:', pos.coords);
-            document.querySelector('.user-pos').innerText =
-                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-        })
-        .catch(err => {
-            console.log('err!!!', err);
-        })
+  getPosition()
+    .then((pos) => {
+      console.log("User position is:", pos.coords);
+      document.querySelector(
+        ".user-pos"
+      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`;
+    })
+    .catch((err) => {
+      console.log("err!!!", err);
+    });
 }
 
 function onPanTo() {
-    console.log('Panning the Map');
-    mapService.panTo(35.6895, 139.6917);
+  console.log("Panning the Map");
+  mapService.panTo(35.6895, 139.6917);
 }
 
-
 function renderPlaces(places) {
-
-    var strHtmls = places.map(function(place) {
-        return `<tr>
+  var strHtmls = places.map(function (place) {
+    return `<tr>
                 <td><img class="icon" src="img/icon.png" ></td>
                 <td>${place.name}</td>
                 <td><button class="remove" onclick="onRemovePlace(${place.id})"> X </button>
-                <td><button class="go" onclick="onGoPlace(${place.id})"> Go! </button></td>
-            </tr>`
-    })
+                <td><button class="go" onclick="onGoPlace(${place.lat},${place.lng})"> Go! </button></td>
+            </tr>`;
+  });
 
-    document.querySelector('tbody').innerHTML = strHtmls.join('');
+  document.querySelector("tbody").innerHTML = strHtmls.join("");
 }
 
 function onRemovePlace(placeId) {
-    removePlace(placeId)
-    var places = loadFromStorage(PLACES_KEY)
-    renderPlaces(places)
-
-
+  removePlace(placeId);
+  var places = loadFromStorage(PLACES_KEY);
+  renderPlaces(places);
 }
 
-function onGoPlace(placeId) {
-
+function onGoPlace(lat, lng) {
+  mapService.panTo(lat, lng);
 }
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -8,21 +8,18 @@ import { storage } from './storage.service.js'
 
 var gIdx = 1;
 
+const LOCS_KEY = 'locationsDB'
 
-const locs = [
-    // { id: 1, name: 'Greatplace', lat: 32.047104, lng: 34.832384, createdAt: 1121255, updatedAt: 1121255 },
-    // { id: 2, name: 'Neveragain', lat: 32.047201, lng: 34.832581, createdAt: 1525255, updatedAt: 1525255 }
-]
+const locs = storage.load(LOCS_KEY) || []
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(locs);
-        }, 2000)
+        }, 0)
     });
 }
 
-const LOCS_KEY = 'locationsDB'
 
 function saveLocations() {
     storage.save(LOCS_KEY, locs)
