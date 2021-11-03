ab8cfe2ff1b710dac6fb4995bb3a352825c8a861
Yahav <freeman.yav@gmail.com>
2021-11-03 13:39:32 +0200

    adding API key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyDrGxjwMxqLAnKCpNpxhGFAHBGxXXtZFuE'; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDrGxjwMxqLAnKCpNpxhGFAHBGxXXtZFuE'; 
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
